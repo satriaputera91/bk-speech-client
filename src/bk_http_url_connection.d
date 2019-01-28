4a8918b307dbf0063c255d300ea125c81032d1f5
satriaputera91 <satriaputera91@gmail.com>
2019-01-28 21:10:50 +0700

    Notula Speech API client library

--- src/bk_http_url_connection.d
+++
@@ -1,6 +0,0 @@
-bk_http_url_connection.o: bk_http_url_connection.c \
- bk_http_url_connection.h bk_utils.h
-
-bk_http_url_connection.h:
-
-bk_utils.h:
--- src/bk_queue_result.d
+++
@@ -1,3 +0,0 @@
-bk_queue_result.o: bk_queue_result.c bk_queue_result.h
-
-bk_queue_result.h:
